9293f98419f357245c001964e9ae92f4fa0162ae
Dan Gorringe <dan.gorringe@hotmail.co.uk>
2017-12-21 13:32:22 +0000

    Added spinboxs for mark allocation, and a mark button

--- src/artifactClass.py
+++ src/artifactClass.py
@@ -1,6 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
 class artifact():
 
-    def __init__(self,question,answer,author,criteriaList,criteriaValues,plagaristBool=False,plagiristAccomplice="none"):
+    def __init__(self,question,answer,author,criteriaList,criteriaValues,plagaristBool=False,plagiristAccomplice="none",markSpinboxs=[]):
         #The question
         self.question = question
         #The answer
@@ -15,6 +18,8 @@ class artifact():
         self.plagaristBool = plagaristBool
         #Who else plagarised
         self.plagiristAccomplice = plagiristAccomplice
+        #List of marks spinboxs
+        self.markSpinboxs = markSpinboxs
 
     def plagirismCheck(self,plagaristBool,plagiristAccomplice):
         if self.plagaristBool == False:
@@ -29,13 +34,33 @@ class artifact():
     def author(self):
         return(self.author)
 
-    def display(self):
+    def display_old(self):
         print("The Question: " + str(self.question))
         print("and is to be marked on:")
         for i in self.criteriaList:
             print(str(i) + " out of an available " + str(i.checkMax()) + " points")
         print("The student submitting is: " + str(self.author))
         print("Their answer is: " + str(self.answer))
+
+    def display(self):
+        artifactWindow = Tk()
+        #Print answer and author
+        w = Label(artifactWindow,text="").pack()
+        w = Label(artifactWindow,text=self.answer).pack()
+        w = Label(artifactWindow,text="By " + self.author).pack()
+        #Add buttons to mark with
+        #for criteria in self.criteriaList:
+        #    marks = StringVar()
+        #    w = Label(artifactWindow,text="Marks for " + str(criteria.title)).pack()
+        #    s = Spinbox(artifactWindow,from_=0,to=criteria.checkMax(),textvariable=marks).pack()
+        for i in range(len(self.criteriaList)):
+            individualMark = IntVar()
+            w = Label(artifactWindow,text="Marks for " + str(self.criteriaList[i].title)).pack()
+            s = Spinbox(artifactWindow,from_=0,to=self.criteriaList[i].checkMax(),textvariable=individualMark)
+            self.markSpinboxs.append(s)
+            s.pack()
+        w = Button(artifactWindow,text="Mark",command=self.mark).pack()
+        artifactWindow.mainloop()
 
     #Mark takes a list of player guesses which correspond to the list of criteria.
     def mark_manual(self):
@@ -56,21 +81,26 @@ class artifact():
                 print("You scored " + str(markingPoints) + " for how you marked " + str(self.criteriaList[i]) + " on " + str(self.author) + "'s piece of work'")
                 return(markingPoints)
 
-        def mark(self,answers):
-            for i in range(len(self.criteriaValues)):
-                if self.criteriaList[i].checkCritical() == True:
-                    print("Criteria is critical")
-                    if answer[i] !=  self.criteriaValues[i]:
-                        print("failed")
-                    if answer[i] ==  self.criteriaValues[i]:
-                        print("passed")
-                #Else the criteria is not crucial, return 'score'
-                else:
-                    #Have the 'score' proportional to the quasi chi-chi (square of the difference
-                    # Score = max - (actual - answer[i])^2
-                    markingPoints = self.criteriaList[i].checkMax() - (self.criteriaValues[i] - answer[i])**2
-                    print("You scored " + str(markingPoints) + " for how you marked " + str(self.criteriaList[i]) + " on " + str(self.author) + "'s piece of work'")
-                    return(markingPoints)
+    def mark(self):
+        for i in range(len(self.criteriaValues)):
+            #print(self.markSpinboxs[i].get())
+            if self.criteriaList[i].checkCritical() == True:
+                print("Criteria is critical")
+                print(str(self.markSpinboxs[i].get()))
+                if int(self.markSpinboxs[i].get()) !=  self.criteriaValues[i]:
+                    print("failed")
+                if int(self.markSpinboxs[i].get()) ==  self.criteriaValues[i]:
+                    print("passed")
+            #Else the criteria is not crucial, return 'score'
+            else:
+                #Have the 'score' proportional to the quasi chi-chi (square of the difference
+                # Score = max - (actual - answer[i])^2
+                markingPoints = self.criteriaList[i].checkMax() - (self.criteriaValues[i] - self.markSpinboxs[i].get())**2
+                print("You scored " + str(markingPoints) + " for how you marked " + str(self.criteriaList[i]) + " on " + str(self.author) + "'s piece of work'")
+                return(markingPoints)
+
+    def printSelf(self):
+        print("button")
 
 
 # Dan Gorringe December 2017
--- src/game.py
+++ src/game.py
@@ -22,15 +22,15 @@ correctnessCriteria = criteriaItem("Correctness","Wether the question has been a
 creativeFlaireCriteria = criteriaItem("Creative Flaire","How artisticly the piece of work has been compeleted",10)
 
 #Create main screen
-#root = Tk()
-#w = Label(root,text="Welcome to Marking Simulator 2018").pack()
+root = Tk()
+w = Label(root,text="Welcome to Marking Simulator 2018").pack()
 
 #Create test artifactToMark
 questionToMarkAnswer = random.randint(1,100)
 questionToMark = questionMathsBasic(questionToMarkAnswer,random.randint(1,100))
 markeeAnswer = questionToMark.markeeAnswer()
-artifactToMark = artifact(questionToMark.display(),markeeAnswer,(nameFirst[random.randint(1,len(nameFirst)-1)] + " " + nameLast[random.randint(1,len(nameLast)-1)]),[correctnessCriteria],[questionToMark.answerMark(markeeAnswer)])
-#Create artifact presentation function
+artifactToMark = artifact(questionToMark.display(),markeeAnswer,(nameFirst[random.randint(1,len(nameFirst)-1)] + " " + nameLast[random.randint(1,len(nameLast)-1)]),[correctnessCriteria],[questionToMark.answerMark(markeeAnswer)]).display()
+#Create artifact presentation function, move this to the class??
 def presentArtifact(artifact):
     artifactWindow = Tk()
     #Print answer and author
@@ -38,14 +38,19 @@ def presentArtifact(artifact):
     w = Label(artifactWindow,text=artifact.answer).pack()
     w = Label(artifactWindow,text="By " + artifact.author).pack()
     #Add buttons to mark with
-    #for criteria in artifact.criteriaList:
-    #    marks = StringVar()
-    #    w = Label(artifactWindow,text="Marks for " + str(criteria.title)).pack()
-    #    w = Spinbox(artifactWindow,from_=0,to=criteria.checkMax(),textvariable=marks).pack()
-    #    w = Button(artifactWindow,test="Mark",command=artifact.mark(marks))
+    for criteria in artifact.criteriaList:
+        marks = StringVar()
+        w = Label(artifactWindow,text="Marks for " + str(criteria.title)).pack()
+        s = Spinbox(artifactWindow,from_=0,to=criteria.checkMax(),textvariable=marks).pack()
+    for i in range(len(artifact.criteriaList)):
+        marks[i] = StringVar()
+        w = Label(artifactWindow,text="Marks for " + str(artifact.criteriaList[i].title)).pack()
+        s = Spinbox(artifactWindow,from_=0,to=artifact.criteriaList[i].checkMax(),textvariable=marks[i]).pack()
+    w = Button(artifactWindow,text="Mark",command=artifact.mark(marks)).pack()
     artifactWindow.mainloop()
 
-presentArtifact(artifactToMark)
+
+#presentArtifact(artifactToMark)
 
 #print(artifactToMark.author)
 
